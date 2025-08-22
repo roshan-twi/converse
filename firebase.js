dec45db0f51d4ae341e105b45b334ac41aaff13b
roshan-twi <159227764+roshan-twi@users.noreply.github.com>
2025-08-22 17:27:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(
-  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString()
+  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString('utf-8')
 );
 
 admin.initializeApp({
