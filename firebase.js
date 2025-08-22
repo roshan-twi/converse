f5b08d500c745935bf28352800be5ca7950a2622
roshan-twi <159227764+roshan-twi@users.noreply.github.com>
2025-08-22 17:05:10 +0530

    Update firebase.js
    to host changes

--- firebase.js
+++ firebase.js
@@ -1,5 +1,8 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-key.json');
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString()
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
